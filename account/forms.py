63afa9adf3ea8531fdb3a74286a66eebb6cc8b36
Ikechukwu Mbakwem <ikembakwem@gmail.com>
2023-03-04 14:17:02 +0100

    Created view for user registration

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class LoginForm(forms.Form):
   username = forms.CharField()
   password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+  password = forms.CharField(label='Password', widget=forms.PasswordInput)
+  password2 = forms.CharField(label='Re-enter your password', widget=forms.PasswordInput)
+
+  class Meta:
+    fields = ('first_name', 'username', 'email')
+    model = User
+
+  def clean_password2(self):
+    cd = self.cleaned_data
+    if cd['password2'] != cd['password']:
+      raise forms.ValidationError('The passwords you entered don\'t match')
+    return cd['password2']
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -33,3 +33,23 @@ def dashboard(request):
   return render(request,
                 'account/dashboard.html',
                 {'section': 'dashboard'})
+
+
+def register(request):
+  if request.method == 'POST':
+    user_form = UserRegistrationForm(request.POST)
+    if user_form.is_valid():
+      # Create a new user object but don't save to database yet
+      new_user = user_form.save(commit=False)
+      # Set the chosen password
+      new_user.set_password(user_form.cleaned_data['password'])
+      # Now save the new user object
+      new_user.save()
+      return render(request,
+                    'account/register_done.html',
+                    {'new_user': new_user})
+  else:
+    user_form = UserRegistrationForm()
+  return render(request,
+                'account/register.html',
+                {'user_form': user_form})
